111d13717df0340da3ff29051344305d7d4cce42
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-09-14 01:39:34 +0600

    ok
    ok

--- IntroductionToDataStructures_CPP/string3.cpp
+++ IntroductionToDataStructures_CPP/string3.cpp
@@ -10,9 +10,77 @@ using namespace std;
 
 int main(void) {
 
+/* shows source code without File line number.
 #ifdef SHOW_SOURCE_CODE
         // We can append this code to any C program
     // such that it prints its source code.
+
+    char c;
+    FILE *fp = fopen(__FILE__, "r");
+
+    do
+    {
+        c = fgetc(fp);
+        putchar(c);
+    }
+    while (c != EOF);
+
+    fclose(fp);
+          // We can append this code to any C program
+    // such that it prints its source code.
+
+#endif
+*/
+#ifdef SHOW_SOURCE_FILE_PATH
+
+       // Prints location of C this C code.
+   printf("%s \n",__FILE__);
+
+#endif
+
+    // A list to store 10 strings, but no memory for the strings themselves.
+    char *strings[10];
+
+    printf("Enter ten lines of code:\n");
+
+    // A line buffer that's sufficiently large.
+    // This will be reused.
+    char line[4096];
+    for ( int i = 0; i < 10; i++) {
+        // Read into the large line buffer.
+        fgets( line, sizeof(line), stdin );
+
+        // Copy the string into a buffer that's just big enough.
+        strings[i] = strdup( line );
+    }
+
+    for ( int i = 0; i < 10; i++) {
+        printf("Line %d: %s\n", i, strings[i]);
+    }
+
+    /*
+* C Program to read and print string using getchar and putchar
+*/
+    char inputString[100], s;
+    int index = 0;
+    printf("Enter a string\n");
+    /* Read string from user using getchar
+     inside while loop */
+    while((s = getchar()) != '\n'){
+        inputString[index] = s;
+        index++;
+    }
+    inputString[index] = '\0';
+    /* Print string stored in inputString using putchar */
+    index = 0;
+    while(inputString[index] != '\0'){
+     putchar(inputString[index]);
+     index++;
+ }
+
+#ifdef SHOW_SOURCE_CODE
+        // We can append this code to any C program
+    // such that it prints its source code with line number.
 
 unsigned long ln = 0;
  FILE *fp = fopen(__FILE__, "r");
@@ -43,26 +111,6 @@ printf("lines num: %lu\n", ln);
 
 #endif
 
-    // A list to store 3 strings, but no memory for the strings themselves.
-    char *strings[10];
-
-    printf("Enter ten lines of code:\n");
-
-    // A line buffer that's sufficiently large.
-    // This will be reused.
-    char line[4096];
-    for ( int i = 0; i < 10; i++) {
-        // Read into the large line buffer.
-        fgets( line, sizeof(line), stdin );
-
-        // Copy the string into a buffer that's just big enough.
-        strings[i] = strdup( line );
-    }
-
-    for ( int i = 0; i < 10; i++) {
-        printf("Line %d: %s\n", i, strings[i]);
-    }
-
-    return 0;
+      return 0;
 }
 
---
+++ IntroductionToDataStructures_CPP/testUntitled1.cpp
@@ -0,0 +1,53 @@
+
+
+#include <stdio.h>
+
+#include <string.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+  // A list to store 3 strings, but no memory for the strings themselves.
+    char *strings[10];
+
+
+
+    printf("Enter ten lines of code:\n");
+
+
+
+    // A line buffer that's sufficiently large.
+
+    // This will be reused.
+
+    char line[4096];
+
+    for ( int i = 0; i < 10; i++) {
+
+        // Read into the large line buffer.
+
+        fgets( line, sizeof(line), stdin );
+
+
+
+        // Copy the string into a buffer that's just big enough.
+
+        strings[i] = strdup( line );
+
+    }
+
+
+
+    for ( int i = 0; i < 10; i++) {
+
+        printf("Line %d: %s\n", i, strings[i]);
+
+    }
+
+    return 0;
+
+}
+
