e8b9b09b2aeda88072619e2ff663d5320c76e7dc
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-09-19 01:43:31 +0600

    ok
    ok

--- getOS.cpp
+++ getOS.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-   #ifdef _WIN32
+
+   #if defined(__MINGW32__) || defined(__CYGWIN__)
       cout<< " Windows"<< endl; system("dir");
    #else
       cout<<"NOT Windows"<< endl; system("ls");
+
    #endif
       return 0;
 }
