cf0d0fa0216298d14af2d3fe71a1089bddc2fbc7
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-10-12 02:10:46 +0600

    ok
    ok

--- ObjectOrientedProgramming_OOP _CPP/CPP_OOP.cpp
+++ ObjectOrientedProgramming_OOP _CPP/CPP_OOP.cpp
@@ -1,6 +1,8 @@
 
 #include<iostream>
 #include<string>
+#include<cstdlib> /// exit()
+/// for linux #include <curses.h> ///erase();
 using namespace std;
 
 
@@ -9,8 +11,9 @@ using namespace std;
 #define SHOW_SOURCE_FILE_PATH
 
 /// Above two lines are user defined Macros
-/// using namespace std;
 
+
+short getOS();
 
 int PausedExitORContinue(string message)
 {
@@ -23,8 +26,8 @@ cout<< '\n'<< message << endl;
         puts("\n Please enter y or Y to continue, or n or N to stop the program ");
 
     cin>> D;
-    puts("You Entered");
-    cout<< D ;
+    puts("You Entered ");
+    cout<< D <<endl;
         if ((D == 'y') || (D == 'Y'))
             {
                     C = 0;
@@ -34,7 +37,7 @@ cout<< '\n'<< message << endl;
         else if((D == 'n') || (D == 'N'))
             {
                 C = 0;
-                return 0;
+                exit(0);
             }
         else
         {
@@ -45,6 +48,12 @@ cout<< '\n'<< message << endl;
     }
 
 }
+
+//clearscreen() /// for linux
+//{
+//    erase();
+//}
+
 
 //class
 class house
@@ -258,9 +267,60 @@ cout << "Battery " << battery<< endl ;
 }
 
 
+
+
+
+class AOO
+{
+    private:
+int weight;
+public:
+
+AOO ( int x = 0)
+{ weight = x; }
+AOO addWeight ( AOO w2 )
+{ AOO temp;
+temp.weight = weight + w2.weight;
+return temp; }
+
+AOO operator + ( AOO w2 )  /// Operator Overloading, +
+{ AOO temp;
+
+temp.weight = weight + w2.weight;
+return temp; }
+
+void operator ++ () /// pre increment operator overloading
+{
+    ++weight ;
+}
+
+void operator ++ (int) /// post increment operator overloading /// int is to differentiate only
+{
+    weight++ ;
+}
+
+int getData()  //printData()
+{ cout<< "Weight " << weight<< endl;  }
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main( )
 {
-
+getOS();
 
 
 #ifdef SHOW_SOURCE_FILE_PATH
@@ -359,7 +419,6 @@ a_obj11.getData(); /// getting value of a object from a overloaded constructor
 
 PausedExitORContinue("Mobile Program");
 
-
 cout<<" \n \
 Write a program, take Phone details as input and store them in object & use \n\
 Constructors. \n\
@@ -381,9 +440,51 @@ doko3.getMobileData();
 
 
 
-PausedExitORContinue("Overloaded Operators  Operator Overloading");
+PausedExitORContinue("Overloaded Operators  Operator Overloading, \n \
+                     When we make operators(+ , - , / , *) work for user-defined types like \n \
+                     objects and structures. This is known as operator overloading ");
 
 
+int a = 63, b = 74, c = 0;
+c=a+b ;
+AOO person1(63);
+
+AOO person2(74);
+AOO total;
+total = person1.addWeight(person2);
+
+total.getData();
+
+total = person2.addWeight( person1 );
+
+
+total.getData();
+
+
+
+total = person1 + person2;
+
+total.getData();
+
+AOO person3;  /// default 0 will be loaded
+
+AOO person4(77);
+
+
+total = person3 + person4;
+
+total.getData();
+
+AOO Jon(77);
+
+++Jon;
+
+Jon.getData();
+
+
+Jon++;
+
+Jon.getData();
 
 
 
@@ -393,3 +494,87 @@ PausedExitORContinue("Overloaded Operators  Operator Overloading");
 return 0;
 }
 
+
+    /// system() is a library function that makes calls to OS commands/programs. system("pause") calls the "pause"
+    /// command in DOS/Windows. Run the pause command in a command shell to see what it does.
+    /// will cause the black dos window (where you see your output) to be paused until you press any key.
+    ///printf("Time taken: %.5fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+/// Shortcut in Code::Blocks
+/// SourceCode Formatter -  Ctrl+Alt+i
+/// line comment  Ctrl+Shift+C
+/// Abbreviations Ctrl+j
+/// open new file  Ctrl+Shift+n
+
+/*
+You can use the OS commands to clear the contents of the console.
+
+#include<stdlib.h>
+int main(){
+
+system("cls");   //For windows
+system("clear"); //For Linux
+
+}
+
+This is actually a quite simple problem. All you have to do is use printf. You don't even need printf or any headers, for that matter.
+
+printf("\e[1;1H\e[2J");
+The \e[1;1H sets the screen to the 1st row and 1st column. the 2J overwrites all characters currently on the screen.
+
+You can also use this:
+
+write(0,"\e[1;1H\e[2J",12);
+Then you don't need stdio.h.
+
+
+*/
+
+/*
+system("pause");
+is wrong because it's part of Windows API and so it won't work in other
+operation systems.
+
+You should try to use just objects from C++ standard library.
+A better solution will be to write:
+
+cin.get(); ///  cin.get();  // or getchar()
+return 0;
+But it will also cause problems if you have other cins in your code.
+ Because after each cin, you'll tap an Enter or \n which is a white space character. cin ignores this character and leaves it in the buffer zone but cin.get(), gets this remained character. So the control of the program reaches the line return 0 and the console gets closed before letting you see the results.
+To solve this, we write the code as follows:
+cin.ignore();
+cin.get();
+return 0;
+
+You can use std::cin.get() from iostream:
+
+#include <iostream> // std::cout, std::cin
+using namespace std;
+
+int main() {
+   do {
+     cout << '\n' << "Press the Enter key to continue.";
+   } while (cin.get() != '\n');
+
+   return 0;
+}
+
+ cin.get();  // or getchar()
+
+Similarly, in C language, getchar() can be used to pause
+ the program without printing the message “Press any key to continue…”.
+
+*/
+
+short getOS()
+{
+       #if defined(_WIN32) || defined(_WIN64) || defined(__TOS_WIN__) || defined(OS_WINDOWS) || defined(__WIN32__) || defined(__WINDOWS__)
+      char windows = 1;
+      cout<< " Windows"<< endl; system("dir");
+
+   #else
+      windows = 0;
+      cout<<"NOT Windows"<< endl;
+   #endif
+
+}
--- getOS.cpp
+++ getOS.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
 
    #if defined(_WIN32) || defined(_WIN64) || defined(__TOS_WIN__) || defined(OS_WINDOWS) || defined(__WIN32__) || defined(__WINDOWS__)
-
+      char windows = 'y'
       cout<< " Windows"<< endl; system("dir");
 
    #else
+      windows = 'n'
       cout<<"NOT Windows"<< endl; system("ls");
 
    #endif
@@ -14,3 +16,16 @@ int main() {
    system("systeminfo");
       return 0;
 }
+
+//short getOS()
+//{
+//       #if defined(_WIN32) || defined(_WIN64) || defined(__TOS_WIN__) || defined(OS_WINDOWS) || defined(__WIN32__) || defined(__WINDOWS__)
+//      char windows = 1;
+//      cout<< " Windows"<< endl; system("dir");
+//
+//   #else
+//      windows = 0;
+//      cout<<"NOT Windows"<< endl;
+//   #endif
+//
+//}
