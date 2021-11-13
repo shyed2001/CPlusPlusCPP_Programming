8f01cd2f3328559db0b6d5661da7876123058ea1
Shyed Shahriar <48835958+shyed2001@users.noreply.github.com>
2021-11-14 02:45:14 +0600

    Create STL_CPP.cpp
    ok

---
+++ CPP_STL/STL_CPP.cpp
@@ -0,0 +1,27 @@
+/*author/owner :== Shyed Shahriar Housaini
+License :== MIT + terms and conditions of author/owner
+Copyright :== author/owner*/
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <stdio.h>
+#include <cstdlib>
+#include <stdlib.h>
+
+
+///using namespace std;    /// bad practice
+
+using std::cout;
+using std::cin;
+using std:: endl;
+using std::string;
+using std::setprecision;
+using std::fixed;
+
+int main()
+{
+
+}
