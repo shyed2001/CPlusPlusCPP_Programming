209ca77f588023a74c8ada049bca3ace181a77a0
shyed2001 <shyed2001@yahoo.com>
2022-06-13 17:58:52 +0600

    Update main.cpp
    ok

--- CPP_Udemy_AbdulBari/main.cpp
+++ CPP_Udemy_AbdulBari/main.cpp
@@ -1143,10 +1143,12 @@ cout<<str[i];
 
 string str="WELCOME";
 for(int i=0; str[i]!=' \0' ; i++)
-
-if(str[i]>=65 && str[i]<=90)
+{
+    if(str[i]>=65 && str[i]<=90)
 {
 str[i]=str[i]+32;
+}
+}
 
 cout< <str<<end1;
 
